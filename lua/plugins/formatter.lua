47525b6bf7dbc14d29b1496abfffaa978898156e
Clément Dalbergue <clement@shortcut-agency.com>
2024-06-03 18:43:45 +0200

    nvim config

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,45 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "eslint_d" },
+				vue = { "eslint_d", "prettier" },
+				html = { "prettier" },
+				htmldjango = { "prettier" },
+				typescript = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			},
+			format_after_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { lsp_fallback = true }
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>f", require("conform").format)
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
+}
