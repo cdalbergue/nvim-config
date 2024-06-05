92e2ca833f254e4e5c3721175b12ac9a70af2993
Clément Dalbergue <clement@shortcut-agency.com>
2024-06-05 10:13:21 +0200

    try new lsp setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,19 @@ return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+		ensure_installed = {
+			"bash",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"vim",
+			"vimdoc",
+			"vue",
+			"typescript",
+			"javascript",
+			"ruby",
+		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
 		highlight = {
